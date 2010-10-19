e8df1d3411f24aedb0506b903704e21e34fa28e9
John Steele <programjsteele@gmail.com>
2010-10-19 15:51:27 -0700

    Finished documentation and prototypes.

--- BSTree.h
+++ BSTree.h
@@ -87,7 +87,7 @@ public:
 
 	//========================retrieve============================ 
 	// Finds the provided object in this tree. Returns NULL if the
-	// item is not found.
+	// item is not found. Calls a recursive helper method. 
 	// 
 	// Preconditions: The Object is not NULL and contains a valid
 	// 		  ASCII character.
@@ -102,8 +102,7 @@ public:
 	// Returns the depth of a character in the tree. The depth
 	// of root is 0.
 	// 
-	// Preconditions: The object should contain a valid ASCII 
-	//		  character.		 
+	// Preconditions: my_root is not NULL. 
 	//
 	// Postconditions: Returns the depth of the character in this
 	//		   tree, returns -1 if not found. 	 
@@ -159,16 +158,17 @@ public:
 
 
 	//========================remove==============================
-	// Removes one occurance of a character from this BSTree. If
-	// it is the last occurence, the Object is removed. 
+	// Removes one occurance of the item from this BSTree. If
+	// it is the last occurence, the Object is removed. Calls a
+	// helper recursive method.
 	// 
 	// Preconditions: the_item is a reference to a non NULL 
 	//		  object. 
 	//
-	// Postconditions: Returns true if the character is found and
+	// Postconditions: Returns true if the item is found and
 	//		   an occurance of it is removed. If it is the
 	//		   last occurence the Object is removed. False
-	// 		   is returned if the character is not found. 
+	// 		   is returned if the item is not found. 
 	//============================================================ 
 	bool remove (const Object &);	
 
@@ -231,7 +231,7 @@ public:
 
 
 private:
-
+	
 	// The Node structure in the BSTree.
 	struct Node {
 		Object *item;   // Object data being stored.
@@ -239,8 +239,166 @@ private:
 		Node *right;    // Right child subtree.
 		int occurances; // Occurances of the character.
 	};
-	
+
 	Node *my_root;	        // The root of this BSTree. 
+
+
+	//========================insertHelper========================
+	// A helper method for instert that recursively inserts the
+	// given Object into this tree. Returns true and inserts the
+	// Object only if its item does not exist in the tree. Other
+	// if found it increments the count and returns false, thus
+	// ownership of the Object remains with the client.
+	//
+	// Preconditions: my_root points to NULL or the first Object 
+	// 		  in this BSTree. 
+	//
+	// Postconditions: Returns true if the_item is insterted in 
+	//		   this BSTree.False is returned if the char-	
+	//		   acter is found, the character count is
+	//		   just incremented. This BSTree only takes 
+	//		   ownership of the Object if the character
+	// 		   is not found.
+	//============================================================ 
+	bool insertHelper (Node *&, Object *);	
+
+
+	//========================retrieveHelper======================
+	// A helper method for retrieve. It recursively finds the 
+	// provided object in this tree. Returns NULL if the item is
+	// not found.
+	// 
+	// Preconditions: The Object is not NULL and has operator< 
+	//  		  implemented correctly to compare values.
+	//		 
+	// Postconditions: Returns a pointer to the Object if found,
+	//	 	   a NULL pointer is returned otherwise. 		 
+	//============================================================ 
+	const Object * retrieveHelper (const Node *root, 
+					const Object &) const;	
+
+
+	//========================removeHelper========================
+	// Removes one occurance of the item from this BSTree. If
+	// it is the last occurence, the Object is removed.
+	// 
+	// Preconditions: the_item is a reference to a non NULL 
+	//		  object. 
+	//
+	// Postconditions: Returns true if the item is found and
+	//		   an occurance of it is removed. If it is the
+	//		   last occurence the Object is removed. False
+	// 		   is returned if the item is not found. 
+	//============================================================ 
+	bool removeHelper (Node *&, const Object &);	
+
+
+	//========================deleteRoot==========================
+	// Deletes the node that the given pointer points to. If 
+	// left and right pointer of the node is not NULL, then we
+	// we delete the item in the node and replace it with the 
+	// most left node in its right subtree.
+	//
+	// Preconditions: root is not NULL.
+	// 		 
+	// Postconditions: the root has been deleted.
+	//============================================================ 
+	void deleteRoot (Node *&);	
+
+
+	//========================findAndDeleteMostLeft===============
+	// Returns the Object item from the node that is located 
+	// at the most left subtree of the given root. 
+	//
+	// Preconditions: root is not NULL.
+	// 		 
+	// Postconditions: Returns a pointer to the Object of the most
+	// 		   left node. That node that previously stored
+	// 		   the item is then deleted.
+	//============================================================ 
+	Object * findAndDeleteMostLeft (Node *&);	
+
+
+	//========================depthHelper=========================
+	// A helper method that recursively calcuates the depth of
+	// the provided Object.
+	//
+	// Preconditions: The Object is not NULL.
+	// 		 
+	// Postconditions: Returns the depth of the given Object.	
+	//		   Returns -1 if the Object is not found.	 
+	//============================================================ 
+	int depthHelper (Node *, const Object &) const;	
+
+
+	//========================descendentsHelper===================
+	// A helper method that calculates the descendents of the
+	// given Object.
+	//
+	// Preconditions: The Object is not NULL.
+	// 		 
+	// Postconditions: Returns the number of descendents the given
+	//		   Object has. Returns -1 if the Object is not
+	//		   found. 
+	//============================================================ 
+	int descendentsHelper (const Node *, const Object &) const;	
+
+
+	//========================countNodes==========================
+	// Counts the number of nodes in the given tree.
+	//
+	// Preconditions: root is not NULL.
+	// 		 
+	// Postconditions: Returns number of nodes in the given tree.
+	//============================================================ 
+	int countNodes (const Node *) const;	
+
+	
+	//========================makeEmptyHelper=====================
+	// Deletes the nodes in the given tree recursively.
+	//
+	// Preconditions: root is not NULL.
+	// 		 
+	// Postconditions: The tree is empty.
+	//============================================================ 
+	void makeEmptyHelper (Node *&) const;	
+
+
+	//========================printHelper=========================
+	// A helper method that recursively prints the the given	
+	// tree in-order.
+	//
+	// Preconditions: root is not NULL.
+	// 		 
+	// Postconditions: The tree is sent to the output.
+	//============================================================ 
+	void printHelper (ostream &, const Node *) const;
+
+
+	//========================clone===============================
+	// Clones the given originalTree into the subtree of copy
+	// recursively.
+	//
+	// Preconditions: origninalTree is not NULL. Object has a 
+	//		  copy-constructor.
+	// 		 
+	// Postconditions: copy now contains its own copies of the
+	//		   nodes contained in the originalTree.
+	//============================================================ 
+	void clone (Node *&, const Node *);
+
+
+	//========================compareHelper=======================
+	// A recursive method that compares two trees. Returns false
+	// if they are not the same. 
+	//
+	// Preconditions: root and other are not NULL.
+	//		  		 
+	// Postconditions: Returns false if the trees are not the 
+	//		   same. 		  
+	//============================================================ 
+	bool compareHelper (const Node *, const Node *) const;
+
 };
 #endif /* _BSTREE_H */
 
